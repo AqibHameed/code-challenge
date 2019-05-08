1bf055f4513a5af3e4d354eebfe03e44056dcb71
AqibHameed <aqibpadana@gmail.com>
2019-05-08 09:40:17 +0200

    remove dublicate code

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -117,13 +117,6 @@ class AssetsController < ApplicationController
 
   def show
     if @asset.present?
-      @number_of_units =  @asset.number_of_units
-      @total_rent = @asset.total_rent
-      @total_area = @asset.total_area
-      @area_rented = @asset.total_area_rented
-      @vaccany = @asset.vacant_area(@total_area)
-      @walt = @asset.calculate_walt(@total_area)
-
       render status: :ok, template: "assets/show.json.jbuilder"
     else
       render status: :unprocessable_entity, json: {errors: "asset must be exist"}
